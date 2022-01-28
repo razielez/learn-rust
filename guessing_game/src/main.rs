c940dc1296b47ddee98d922c3f318aff96bfff6d
razielez <linran@live.com>
2022-01-28 16:18:05 +0800

    feat: add guessing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let sercret_num = rand::thread_rng().gen_range(1..101);
+    loop {
+        println!("Please input guess.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed:{} ", guess);
+        match guess.cmp(&sercret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+            }
+        }
+    }
 }
